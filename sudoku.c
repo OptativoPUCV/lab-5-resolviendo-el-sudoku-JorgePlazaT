8e1cab2fa040789c6387bdd9dff30866697768b0
Your Name <you@example.com>
2025-05-18 18:31:56 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -45,7 +45,7 @@ void print_node(Node* n){
 
 int is_valid(Node* n){
   int visitado[10]; //revisar columnas, filas o submatriz
-//ver que no se repita ninguna fila 
+//ver que no se repita ningun numero en las filas
   for(int i = 0; i < 9; i++){
     for(int k = 0; k < 10;k++) visitado[k] = 0;
     for(int j = 0; j < 9; j++){ //recorrer cada fila de i
@@ -56,9 +56,9 @@ int is_valid(Node* n){
       }
     }
   }
-//ver que no se repita ninugna columna
+//ver que no se repita ningun numero en las columas
   for(int j = 0; j < 9; j++){
-    for(int k = 0; k < 10;k++) visitado[k] = 0;
+          int visitado[10] = {0};
     for(int i = 0; i < 9; i++){ 
       int valor = n->sudo[i][j];
       if(valor != 0){
@@ -67,6 +67,12 @@ int is_valid(Node* n){
       }
     }
   }
+/*
+  for(int columna = 0; columna < 3; columna++){
+    for(int fila = 0; fila < 3; fila++){
+    }
+  }
+*/
 
 
 
